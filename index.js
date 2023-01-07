54f9962d846a426590e74e9bee266c34820942d4
EmersonMedina <emermedina877@gmail.com>
2023-01-06 22:23:18 -0600

    Modificando configuración de cookies

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import "dotenv/config";
 import "./database/connectdb.js";
+import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors";
 
 import authRouter from "./routes/auth.route.js";
 import linkRouter from "./routes/link.route.js";
-import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -26,8 +26,10 @@ app.use(
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/links", linkRouter);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
